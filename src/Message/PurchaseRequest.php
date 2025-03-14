9d875105160510e3fe9b125b3bb159c7a17e4b1e
Álvaro Codina <alvarocodinalahoz@gmail.com>
2025-03-14 08:23:34 +0100

    fix: rename param Ds_Merchant_MerchantDescriptor

--- src/Message/PurchaseRequest.php
+++ src/Message/PurchaseRequest.php
@@ -24,7 +24,7 @@ class PurchaseRequest extends AbstractRequest
             'Ds_Merchant_MerchantUrl' => $this->getNotifyUrl(),
             // optional fields
             'Ds_Merchant_ProductDescription' => $this->getDescription(),
-            'Ds_Merchant_MerchantDescription' => substr($this->getDescription(), 0, 25),
+            'Ds_Merchant_MerchantDescriptor' => substr($this->getDescription(), 0, 25),
             'Ds_Merchant_Cardholder' => $this->getCardholder(),
             'Ds_Merchant_UrlOK' => $this->getReturnUrl(),
             'Ds_Merchant_UrlKO' => $this->getCancelUrl() ?: $this->getReturnUrl(),
